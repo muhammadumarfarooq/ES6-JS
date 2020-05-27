d3936cf8d108501c79671dc5f81a208b0b77ba28
muhammadumarfarooq <develop.umar@gmail.com>
2020-05-27 13:34:08 +0500

    map helper

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //Array Helpers
 
-//forEach
+//===== forEach Helper =====
 var colors = ["red", "green", "blue"];
 
 //ES5
-for(var i = 0; i < colors.length; i++){
+for ( var i = 0; i < colors.length; i++ ) {
   console.log(colors[i]);
 }
 
 //ES6
-colors.forEach(function(color){
-  console.log(color);
+colors.forEach(function (color) {
+  // console.log(color);
 });
 
 // forEach Advanced Use
@@ -20,13 +20,31 @@ var numbers = [3, 5, 6, 10];
 
 var sum = 0;
 
-numbers.forEach(function(number){
-    sum += number;
+numbers.forEach(function (number) {
+  sum += number;
 });
 
-console.log(sum);
+// console.log(sum);
 
+//===== Map Helper =====
 
+var numbersSingle = [2, 4, 6];
+var numbersDouble = [];
+
+//ES5
+for ( var i = 0; i < numbersSingle.length; i++ ) {
+  numbersDouble.push(numbersSingle[i] * 2);
+}
+
+console.log(numbersDouble);
+
+//ES6
+
+const newNumbersDouble = numbersSingle.map(function (number) {
+  return number * 2;
+});
+
+console.log(newNumbersDouble);
 
 
 
