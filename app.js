661e38261a56031b581aa95ad70e393b7da1957d
muhammadumarfarooq <develop.umar@gmail.com>
2020-05-27 12:55:51 +0500

    forEach Advanced use case

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-//Array Helpers forEach
+//Array Helpers
 
+//forEach
 var colors = ["red", "green", "blue"];
 
 //ES5
@@ -11,3 +12,23 @@ for(var i = 0; i < colors.length; i++){
 colors.forEach(function(color){
   console.log(color);
 });
+
+// forEach Advanced Use
+//sum of elements of an array using forEach
+
+var numbers = [3, 5, 6, 10];
+
+var sum = 0;
+
+numbers.forEach(function(number){
+    sum += number;
+});
+
+console.log(sum);
+
+
+
+
+
+
+
